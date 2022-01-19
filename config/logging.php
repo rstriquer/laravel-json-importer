bb9aadc6ccde5069ee466424e95ffdf7c799dc52
Ricardo Soares <rstriquer@gmail.com>
2022-01-18 23:39:02 -0300

    Define cronjobs special log file

--- config/logging.php
+++ config/logging.php
@@ -60,6 +60,12 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
+        'cronjobs' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/cronjobs.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
